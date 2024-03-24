b105c7178bcb333e0fdb748095077dd3a7b289d5
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-25 02:57:10 +0300

    debugg

---
+++ src/00-objectball.js
@@ -0,0 +1,221 @@
+
+function gameObject() {
+    return {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
+                },
+                "Brook Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                "Jason Terry": {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1
+                }
+            }
+        },
+        away: {
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
+                },
+                "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12
+                }
+            }
+        }
+    };
+}
+
+console.log(gameObject());
+
+function numPointsScored(playerName) {
+    let object = gameObject();
+    for (let team in object) {
+        if (object[team].players.hasOwnProperty(playerName)) {
+            return object[team].players[playerName].points;
+        }
+    }
+    return "Player not found";
+}
+
+function shoeSize(playerName) {
+    let object = gameObject();
+    for (let team in object) {
+        if (object[team].players.hasOwnProperty(playerName)) {
+            return object[team].players[playerName].shoe;
+        }
+    }
+    return "Player not found";
+}
+
+function teamColors(teamName) {
+    let object = gameObject();
+    for (let team in object) {
+        if (object[team].teamName === teamName) {
+            return object[team].colors;
+        }
+    }
+    return "Team not found";
+}
+
+function teamNames() {
+    let object = gameObject();
+    return [object.home.teamName, object.away.teamName];
+}
+
+function playerNumbers(teamName) {
+    let object = gameObject();
+    for (let team in object) {
+        if (object[team].teamName === teamName) {
+            return Object.values(object[team].players).map(player => player.number);
+        }
+    }
+    return "Team not found";
+}
+
+function playerStats(playerName) {
+    let object = gameObject();
+    for (let team in object) {
+        if (object[team].players.hasOwnProperty(playerName)) {
+            return object[team].players[playerName];
+        }
+    }
+    return "Player not found";
+}
+
+function bigShoeRebounds() {
+    let object = gameObject();
+    let largestShoeSize = 0;
+    let playerWithLargestShoe = "";
+    for (let team in object) {
+        for (let player in object[team].players) {
+            if (object[team].players[player].shoe > largestShoeSize) {
+                largestShoeSize = object[team].players[player].shoe;
+                playerWithLargestShoe = player;
+            }
+        }
+    }
+    return object.home.players[playerWithLargestShoe].rebounds || object.away.players[playerWithLargestShoe].rebounds;
+}
+
+function doesLongNameStealATon() {
+    let object = gameObject();
+    
+    // Step 1: Find the player with the longest name
+    let longestName = "";
+    for (let team in object) {
+        for (let player in object[team].players) {
+            if (player.length > longestName.length) {
+                longestName = player;
+            }
+        }
+    }
+    
+    // Step 2: Find the player with the most steals
+    let mostStealsPlayer = "";
+    let mostSteals = 0;
+    for (let team in object) {
+        for (let player in object[team].players) {
+            if (object[team].players[player].steals > mostSteals) {
+                mostSteals = object[team].players[player].steals;
+                mostStealsPlayer = player;
+            }
+        }
+    }
+    
+    // Step 3: Compare the players found in steps 1 and 2
+    return longestName === mostStealsPlayer;
+}
+
+// Test the function
+console.log(doesLongNameStealATon());
